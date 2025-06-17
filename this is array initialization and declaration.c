ae7c5317c13d382730304bf6247aa8d800004835
frenzy irimu gatheru <frenzyzyfre@gmail.com>
2025-06-17 21:19:49 +0300

    Add files via upload

---
+++ this is array initialization and declaration.c
@@ -0,0 +1,14 @@
+/*NAME Frenzy gatheru
+REG NO BSE-05-05163/2024
+this is array initialization and declaration
+*/
+#include<stdio.h>
+
+int main(){
+	int scores[6]={30,40,50,60,70,80};//array initialization and declaration
+	
+	printf("the socore are as follows %d",scores[0]);
+	
+	return 0;
+	
+}
